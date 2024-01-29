ea964a889045882e77f367365794ab53353637f3
Hsuan1128 <dewitt1128@gmail.com>
2024-01-30 01:16:14 +0800

    login page edit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "backLogin",
+      component: () => import("../views/backLogin.vue"),
+      meta: {
+        title: "管理員登入",
+      },
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/AboutView.vue"),
+    },
+  ],
+});
+//跳轉頁面時跳到頂端
+router.afterEach(() => {
+  window.scrollTo(0, 0);
+});
 
-export default router
+// 產生每一頁頁籤的title
+router.beforeEach(async (to, from) => {
+  if (to.meta && to.meta.title) {
+    document.title = to.meta.title;
+  }
+});
+export default router;
---
+++ src/views/backLogin.vue
@@ -0,0 +1,79 @@
+<template>
+  <main class="loginMain">
+    <section class="inputArea">
+      <img src="@/assets/images/login/logoG.svg" alt="logoG" class="logoG" />
+      <img src="@/assets/images/login/cloudb.svg" alt="cloudb" class="cloudb" />
+      <img
+        src="@/assets/images/login/cloudsun.svg"
+        alt="cloudsun"
+        class="cloudsun"
+      />
+      <div class="meerkats">
+        <img
+          src="../assets/images/login/vetor_animal_meerkat.svg"
+          alt="meerkat"
+          class="meerkat"
+        />
+        <img
+          src="../assets/images/login/vetor_animal_meerkat.svg"
+          alt="meerkat"
+          class="meerkat"
+        />
+        <img
+          src="../assets/images/login/vetor_animal_meerkat.svg"
+          alt="meerkat"
+          class="meerkat"
+        />
+      </div>
+      <div class="acc">
+        <div class="img">
+          <img
+            src="@/assets/images/login/icon/account.svg"
+            alt="account"
+            class="account"
+          />
+        </div>
+
+        <input type="text" placeholder="帳號" class="account" />
+      </div>
+      <div class="psw">
+        <div class="img">
+          <img src="@/assets/images/login/icon/psw.svg" alt="psw" class="psw" />
+        </div>
+        <input type="password" placeholder="密碼" />
+      </div>
+      <button type="submit" class="defaultBtn pcInnerText">
+        登入
+        <img src="@/assets/images/login/icon/btnArrow.svg" alt="" />
+      </button>
+    </section>
+
+    <section class="grassArea">
+      <img
+        src="../assets/images/login/nature_tree_3.svg"
+        alt="tree3"
+        class="tree3"
+      />
+      <img
+        src="../assets/images/login/nature_tree_8.svg"
+        alt="tree8"
+        class="tree8"
+      />
+      <img
+        src="../assets/images/login/nature_grasses_5.svg"
+        alt="grasses5"
+        class="grasses5"
+      />
+      <img
+        src="../assets/images/login/nature_grasses_7.svg"
+        alt="grasses7"
+        class="grasses7"
+      />
+      <img
+        src="@/assets/images/login/Group 1562.svg"
+        alt="grass"
+        class="grass"
+      />
+    </section>
+  </main>
+</template>
