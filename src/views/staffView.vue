7e15bce849dd489c5c33aa5a7858cd51f5889536
Hsuan1128 <dewitt1128@gmail.com>
2024-02-03 14:55:37 +0800

    add iview

--- src/views/staffView.vue
+++ src/views/staffView.vue
@@ -15,29 +15,33 @@
         </div>
       </div>
       <div class="formArea">
-        <table class="formTable">
-          <thead>
-            <tr>
-              <th v-for="title in titles" :key="title" class="pcInnerText">
-                {{ title }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="list in lists" class="pcInnerText">
-              <td>{{ list.num }}</td>
-              <td>{{ list.title }}</td>
-              <td>{{ list.email }}</td>
-              <td>{{ list.acc }}</td>
-              <td>{{ list.psw }}</td>
-              <td><Switch /></td>
-              <td>
-                <img :src="list.edit" alt="icon" />
-                <img v-if="list.remove" :src="list.remove" alt="icon" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Table stripe :columns="columns" :data="data" ref="table">
+          <template #name="{ row }">
+            <strong> {{ row.name }}</strong>
+          </template>
+          <template #status="{ row }">
+            <Switch v-model="row.active" />
+          </template>
+          <template #action="{ row, index }">
+            <Button
+              type="primary"
+              class="trash"
+              size="small"
+              style="margin-right: 5px"
+              @click="show(index)"
+              ><img src="../assets/images/formicon/revise.svg" alt=""
+            /></Button>
+            <Button
+              type="error"
+              class="trash"
+              size="small"
+              @click="remove(index)"
+              ><img src="../assets/images/formicon/delete.svg" alt="" /></Button
+          ></template>
+        </Table>
+        <template>
+          <Page :total="100" />
+        </template>
       </div>
       <div class="add">
         <img src="@/assets/images/formicon/plus.svg" alt="add" class="add" />
@@ -53,40 +57,84 @@
 import sidebar from "@/components/sidebar.vue";
 import Switch from "@/components/switch.vue";
 import grass from "@/components/grass.vue";
-const revise = new URL("../assets/images/formicon/revise.svg", import.meta.url)
-  .href;
-const rm = new URL("../assets/images/formicon/delete.svg", import.meta.url)
-  .href;
+import { Table, Page } from "view-ui-plus";
 export default {
   data() {
     return {
-      titles: ["No.", "職位", "信箱", "帳號", "密碼", "狀態", "刪改"],
-      lists: [
+      columns: [
         {
-          num: "1",
-          title: "超級管理員",
-          email: "test@gmail.com",
-          acc: "test",
-          psw: "123",
-          edit: revise,
-          remove: "",
+          title: "編號",
+          key: "sta_id",
+          align: "center",
         },
         {
-          num: "2",
-          title: "管理員",
-          email: "test@gmail.com",
-          acc: "test",
-          psw: "123",
-          edit: revise,
-          remove: rm,
+          title: "職位",
+          key: "sta_pos",
+          width: 200,
+          align: "center",
+        },
+        {
+          title: "信箱",
+          key: "sta_email",
+          width: 180,
+          align: "center",
+        },
+        {
+          title: "帳號",
+          key: "sta_acc",
+        },
+        {
+          title: "密碼",
+          key: "sta_psw",
+
+          align: "center",
+        },
+        {
+          title: "狀態",
+          slot: "status",
+
+          align: "center",
+        },
+        {
+          title: "刪改",
+          slot: "action",
+          align: "center",
+        },
+      ],
+      data: [
+        {
+          sta_id: "1",
+          sta_pos: "超級管理員",
+          sta_email: "test@gmail.com",
+          sta_acc: "test",
+          sta_psw: "123",
+        },
+        {
+          sta_id: "2",
+          sta_pos: "管理員",
+          sta_email: "test@gmail.com",
+          sta_acc: "test",
+          sta_psw: "123",
         },
       ],
     };
+  },
+  methods: {
+    remove(index) {
+      this.data.splice(index, 1);
+    },
   },
   components: {
     sidebar,
     Switch,
     grass,
+    Table,
   },
 };
 </script>
+<style>
+.trash {
+  border: transparent;
+  background-color: transparent;
+}
+</style>
