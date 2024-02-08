1938f1964f94812b9098553e2c5530ecce9f2a1d
yhkekeke <diandian2510@gmail.com>
2024-02-08 23:32:59 +0800

    create components switchXL

---
+++ src/components/switchXL.vue
@@ -0,0 +1,44 @@
+<template>
+  <Space direction="vertical">
+    <Space>
+      <Switch
+        size="large"
+        true-color="#13ce66"
+        false-color="#ff9900"
+        v-model="value"
+      >
+        <template #open>
+          <span>已用票</span>
+        </template>
+        <template #close>
+          <span>未用票</span>
+        </template>
+      </Switch>
+    </Space>
+  </Space>
+</template>
+
+<script>
+export default {
+  props:{
+    status: {
+        type: String,
+        required: true,
+    },
+  },
+  data() {
+    return {
+      value: true,
+    };
+  },
+};
+</script>
+<style lang="scss">
+.ivu-switch-large {
+  width: 66px;
+  height: 23px;
+}
+.ivu-switch-large.ivu-switch-checked:after{
+  left: 45px;
+}
+</style>
