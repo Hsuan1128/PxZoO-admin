0140319e7aa32ca4b53b043f12ca3e380e34f8ac
wenchieh0119 <jamie1063021@gmail.com>
2024-02-27 19:58:48 +0800

    update question

--- src/components/questionrevise.vue
+++ src/components/questionrevise.vue
@@ -17,17 +17,22 @@
                 <input type="file" id="question_img_a"  :placeholder="question_img_a" @change="handleFileChange('question_img_a', $event)">
             </div> -->
 
-                <div class="Revise_content_align">
+                <div class="Revise_content_align revise_img">
                     <label for="" class="pcInnerText">圖片A</label>
-                    <div>
-                        <p v-if="rowdata.question_img_a && !imageChanged.a">{{ rowdata.question_img_a }}</p>
-                        <span v-else-if="!isNewImage.a">{{ uploadImagePlaceholder }}</span>
-                        <input type="file" id="question_img_a" @change="handleFileChange('question_img_a', $event)">
-
-
-                    </div>
+                    <label class="pcInnerText revise_imgtext ">
+                        <span v-if="file_a">{{ file_a }}</span>
+                        <span v-else>{{ rowdata.question_img_a }}</span>
+                        <div class="iconBtn pcInnerText">
+                            <p class="iconText">
+                                <img src="@/assets/images/login/icon/image.svg" alt="" class="buttonIcon" />
+                                選擇檔案
+                            </p>
+                            <img src="@/assets/images/login/icon/btnArrow.svg" alt="" class="arrowIcon" />
+                        </div>
+                        <input type="file" id="question_img_a" name="news_pic" accept="image/png, image/jpeg"
+                            @change="handleFileChange('question_img_a', $event, 'question_img_a')">
+                    </label>
                 </div>
-                
 
                 <div class="Revise_content_align">
                     <label for="" class="pcInnerText">選項B</label>
@@ -39,44 +44,62 @@
                 <label for="" class="pcInnerText">圖片B</label>
                 <input type="file" id="question_img_b" :placeholder="rowdata.question_img_b" @change="handleFileChange('question_img_b', $event)">
             </div> -->
-                <div class="Revise_content_align">
+                  <div class="Revise_content_align revise_img">
                     <label for="" class="pcInnerText">圖片B</label>
-                    <div>
-                        <p v-if="rowdata.question_img_b && !imageChanged.b">{{ rowdata.question_img_b}}</p>
-                        <span v-else-if="!isNewImage.b">{{ uploadImagePlaceholder }}</span>
-                        <input type="file" id="question_img_b" @change="handleFileChange('question_img_b', $event)">
-                        <!-- <label for="question_img_b">修改圖片</label> -->
-
-                    </div>
+                    <label class="pcInnerText revise_imgtext ">
+                        <span v-if="file_b">{{ file_b }}</span>
+                        <span v-else>{{ rowdata.question_img_b }}</span>
+                        <div class="iconBtn pcInnerText">
+                            <p class="iconText">
+                                <img src="@/assets/images/login/icon/image.svg" alt="" class="buttonIcon" />
+                                選擇檔案
+                            </p>
+                            <img src="@/assets/images/login/icon/btnArrow.svg" alt="" class="arrowIcon" />
+                        </div>
+                        <input type="file" id="question_img_b" name="news_pic" accept="image/png, image/jpeg"
+                            @change="handleFileChange('question_img_b', $event, 'question_img_b')">
+                    </label>
                 </div>
                 <div class="Revise_content_align">
                     <label for="" class="pcInnerText">選項C</label>
                     <input type="text" v-model="rowdata.question_option_c" :placeholder="question_option_c">
                 </div>
-                <div class="Revise_content_align">
+                <div class="Revise_content_align revise_img">
                     <label for="" class="pcInnerText">圖片C</label>
-                    <div>
-                        <p v-if="rowdata.question_img_c && !imageChanged.c">{{ rowdata.question_img_c}}</p>
-                        <span  v-else-if="!isNewImage.c">{{ uploadImagePlaceholder }}</span>
-                        <input type="file" id="question_img_a" @change="handleFileChange('question_img_c', $event)">
-
-
-                    </div>
+                    <label class="pcInnerText revise_imgtext ">
+                        <span v-if="file_c">{{ file_c }}</span>
+                        <span v-else>{{ rowdata.question_img_c }}</span>
+                        <div class="iconBtn pcInnerText">
+                            <p class="iconText">
+                                <img src="@/assets/images/login/icon/image.svg" alt="" class="buttonIcon" />
+                                選擇檔案
+                            </p>
+                            <img src="@/assets/images/login/icon/btnArrow.svg" alt="" class="arrowIcon" />
+                        </div>
+                        <input type="file" id="question_img_c" name="news_pic" accept="image/png, image/jpeg"
+                            @change="handleFileChange('question_img_c', $event, 'question_img_c')">
+                    </label>
                 </div>
 
                 <div class="Revise_content_align">
                     <label for="" class="pcInnerText">選項D</label>
                     <input type="text" v-model="rowdata.question_option_d" :placeholder="question_option_d">
                 </div>
-                <div class="Revise_content_align">
-                    <label for="" class="pcInnerText">圖片D</label>
-                    <div>
-                        <p v-if="rowdata.question_img_d && !imageChanged.d">{{ rowdata.question_img_d }}</p>
-                        <span  v-else-if="!isNewImage.d">{{ uploadImagePlaceholder }}</span>
-                        <input type="file" id="question_img_d" @change="handleFileChange('question_img_d', $event)">
-
-
-                    </div>
+                <div class="Revise_content_align revise_img">
+                    <label for="" class="pcInnerText">圖片A</label>
+                    <label class="pcInnerText revise_imgtext ">
+                        <span v-if="file_d">{{ file_d }}</span>
+                        <span v-else>{{ rowdata.question_img_d }}</span>
+                        <div class="iconBtn pcInnerText">
+                            <p class="iconText">
+                                <img src="@/assets/images/login/icon/image.svg" alt="" class="buttonIcon" />
+                                選擇檔案
+                            </p>
+                            <img src="@/assets/images/login/icon/btnArrow.svg" alt="" class="arrowIcon" />
+                        </div>
+                        <input type="file" id="question_img_d" name="news_pic" accept="image/png, image/jpeg"
+                            @change="handleFileChange('question_img_d', $event, 'question_img_d')">
+                    </label>
                 </div>
 
                 <div class="Revise_content_align">
@@ -116,41 +139,45 @@ import Confirm_question from "@/components/confirm_question.vue";
 
 export default {
     props: {
-        ReviseSwitch: false,
+        ReviseSwitch: {
+            type: Boolean,
+            default: false
+        },
         rowdata: {
             type: Object,
             required: true,
-
+            default: () => ({})
         }
     },
     data() {
         return {
-            isNewImage: {
-                a: false,
-                b: false,
-                c: false,
-                d: false,
-            },
-             // 添加用於追蹤圖片是否已更改的變量
-             imageChanged: {
-                a: false, // 初始化為 false，表示圖片尚未更改
-                b: false,
-                c: false,
-                d: false,
-            },
+            // isNewImage: {
+            //     a: false,
+            //     b: false,
+            //     c: false,
+            //     d: false,
+            // },
+            // 添加用於追蹤圖片是否已更改的變量
+            // imageChanged: {
+            //     a: false, // 初始化為 false，表示圖片尚未更改
+            //     b: false,
+            //     c: false,
+            //     d: false,
+            // },
+
 
             ConfirmSwitch: false,
-            question_text: '',
-            question_option_a: '',
-            question_img_a: null,
-            question_option_b: '',
-            question_img_b: '',
-            question_option_c: '',
-            question_img_c: '',
-            question_option_d: '',
-            question_img_d: '',
-            question_correctanswer: '',
-            question_answer_illustrate: '',
+            question_text: this.rowdata.question_text,
+            question_option_a:this.rowdata.question_option_a,
+            question_img_a: this.rowdata.question_img_a,
+            question_option_b:this.rowdata.question_option_b,
+            question_img_b:this.rowdata.question_img_b,
+            question_option_c: this.rowdata.question_option_c,
+            question_img_c:this.rowdata.question_img_c,
+            question_option_d:this.rowdata.question_option_d,
+            question_img_d:this.rowdata.question_img_d,
+            question_correctanswer:this.rowdata.question_correctanswer,
+            question_answer_illustrate:this.rowdata.question_answer_illustrate,
             confirmData: new FormData(),
             uploadImagePlaceholder: '請上傳圖片',
 
@@ -161,24 +188,28 @@ export default {
             //從這個組件傳送控制修改彈窗的顯示/隱藏參數數值
             this.$emit('update-switch', !this.ReviseSwitch);
         },
-        handleFileChange(field, event) {
+        handleFileChange(field, event,fileType) {
             const file = event.target.files[0];
             // 將文件資料保存到對應的 rowdata 屬性中
-           
+        
 
             // 如果您只需要保存檔案名稱，請使用下面的代碼
             this.rowdata[field] = file;
-            // 將相應的 imageChanged 設置為 true，表示圖片已更改
-            this.imageChanged[field.split('_').pop()] = true;
-            // 將相應的 isNewImage 設置為 true，表示新圖片已選擇
-            this.isNewImage[field.split('_').pop()] = true;
-    //         const fileName = file.name;
-    
-    // // 將文件資料保存到對應的 rowdata 屬性中
-    // this.rowdata[field] = fileName;
-            console.log(file)
-            // 顯示上傳的文件名（或其他信息）
-            console.log(`${field} uploaded: ${file.name}`);
+            
+         // 根據文件類型更新相應的文件名
+         if (fileType === 'question_img_a') {
+                this.file_a = this.rowdata[field].name;
+                console.log(this.file_a);
+            } else if (fileType === 'question_img_b') {
+                this.file_b = this.rowdata[field].name;
+                console.log(this.file_b);
+            }else if (fileType === 'question_img_c') {
+                this.file_c = this.rowdata[field].name;
+                console.log(this.file_c);
+            }else if (fileType === 'question_img_d') {
+                this.file_d = this.rowdata[field].name;
+                console.log(this.file_d);
+            }
         },
 
         prepareConfirmData() {
@@ -189,11 +220,10 @@ export default {
             }
             //把資料傳送到newsConfirm的組件
             this.confirmData = formData
-            console.log(this.confirmData)
+            // console.log(this.confirmData)
         },
-        questionRevise(){
-            console.log("Question Revise method called");
-    console.log("ConfirmSwitch value:", this.ConfirmSwitch);
+        questionRevise() {
+
             if (this.rowdata.question_text != this.question_text ||
                 this.rowdata.question_option_a != this.question_option_a ||
                 this.rowdata.question_img_a != this.question_img_a ||
@@ -207,12 +237,13 @@ export default {
                 this.rowdata.question_answer_illustrate != this.question_answer_illustrate
             ) {
                 this.ConfirmSwitch = !this.ConfirmSwitch;
-           
+
                 this.prepareConfirmData()
-            
+
             } else {
                 this.updateReviseSwitch();
-                    // window.location.reload();
+                this.$emit('trigger-update-revise-switch');
+                // window.location.reload();
             }
         },
         transferData() {
@@ -243,22 +274,22 @@ export default {
                 // this.ConfirmSwitch = !this.ConfirmSwitch;
                 this.prepareConfirmData()
 
-             axios.post(`${import.meta.env.VITE_API_URL}/questionRevise.php`, this.confirmData,{
+                axios.post(`${import.meta.env.VITE_API_URL}/questionRevise.php`, this.confirmData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     },
                 })
-                .then((res) => {
-                    console.log(res)
-                    console.log('修改已送出');
-                    //關閉修改的彈窗
-                    this.$emit('trigger-update-revise-switch')
-                    //重新整理頁面讓資料更新成更改後的
-                    window.location.reload();
-                })
-                .catch(error => {
-                    console.error('更新錯誤:', error);
-                });
+                    .then((res) => {
+                        console.log(res)
+                        console.log('修改已送出');
+                        //關閉修改的彈窗
+                        this.$emit('trigger-update-revise-switch')
+                        //重新整理頁面讓資料更新成更改後的
+                        window.location.reload();
+                    })
+                    .catch(error => {
+                        console.error('更新錯誤:', error);
+                    });
             } else {
                 this.updateReviseSwitch();
             }
@@ -270,7 +301,7 @@ export default {
     components: {
         Confirm_question,
     },
-    
+
 };
 </script>
   
@@ -295,11 +326,14 @@ export default {
 
         h1 {
             margin-top: 2vw;
+            text-align: center;
             color: #11A729;
         }
-        .pcInnerText{
+
+        .pcInnerText {
             color: #3F3F3F;
         }
+
         .Revise_content {
             width: 60.82vw;
             margin: 3vw auto 0;
@@ -313,7 +347,7 @@ export default {
                 width: 100%;
 
                 label {
-                    margin-right: 20px;
+                    // margin-right: 20px;
                     display: block;
                     width: 15%;
                 }
@@ -325,6 +359,10 @@ export default {
                     border: none;
                 }
 
+                input[type="file"] {
+                    display: none;
+                }
+
                 p {
                     width: 85%;
                     height: 2vw;
@@ -332,23 +370,78 @@ export default {
                     border: none;
                 }
 
+                .revise_text {
+                    padding: 0.5vw 0.5vw;
+                    background-color: #fff;
+                    border: 2px #3F3F3F solid;
+                    width: 100%;
+                    height: 2.5vw;
+                    outline: none;
+                    vertical-align: top;
+                    border-radius: 0;
+                    appearance: none;
+                    -moz-appearance: none;
+                    -webkit-appearance: none;
+
+                    &::placeholder {
+                        color: #CCC;
+                    }
+                }
+
+                .revise_imgtext {
+                    display: flex;
+                    justify-content: space-between;
+                    width: 100%;
+
+                    .iconBtn {
+                        width: 140px;
+                    }
+                }
+
+
                 .Revise_textarea {
-                    width: 85%;
+                    padding: 0.5vw 0.5vw;
+                    background-color: #fff;
+                    border: 2px #3F3F3F solid;
+                    width: 100%;
                     height: 10vw;
+                    outline: none;
+                    vertical-align: top;
+                    border-radius: 0;
+                    appearance: none;
+                    -moz-appearance: none;
+                    -webkit-appearance: none;
                     resize: none;
-                    padding-inline: 8px;
-                    border: none;
+
+                    &::placeholder {
+                        color: #CCC;
+                    }
+                }
+
+                select {
+                    padding: 0.5vw 0.5vw;
+                    background-color: #fff;
+                    border: 2px #3F3F3F solid;
+                    width: 100%;
+                    height: 2.5vw;
+                    outline: none;
+                    vertical-align: top;
+                    border-radius: 0;
+                    resize: none;
+
+                    &::placeholder {
+                        color: #CCC;
+                    }
                 }
             }
         }
 
         .Revise_btns {
             width: 280px;
-            margin: 3vw auto ;
+            margin: 3vw auto;
             display: flex;
             justify-content: space-between;
         }
     }
-}
-</style>
+}</style>
   
